cd58b2623e0caf311364a87cdbe1c08026bd7036
Andrew Thomson <Thomson.w.Andrew@gmail.com>
2017-06-23 13:43:11 -0600

    refactored user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,74 @@
+"use strict";
+
+const express     = require('express');
+const router      = express.Router();
+const session     = require('express-session');
+const request     = require('request');
+
+//Bring needed models into scope.
+const Users       = require("../database/models/Users");
+const Topics      = require("../database/models/Topics");
+const db          = require("../utilities/DB_helpers.js")
+
+//Token and security
+const jwt         = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+require('dotenv').config();
+
+//USER LOGIN ROUTE
+router.post("/login", (req, res) => {
+  if(req.body.email && req.body.password){
+    const email = req.body.email.toLowerCase();
+    const password = req.body.password;
+    Users.findOne({'email': email })
+      .then( (user) => {
+        if( !user) {
+          res.status(401).json({message:"No such user found."});
+        } else if( user && (bcrypt.compareSync(  password, user.password))) {
+          const payload = {id: user.id};
+          const token = jwt.sign(payload, process.env.APP_SECRET_KEY);
+          res.json({message: "Password OK.", token: token});
+        } else {
+          res.status(401).json({message:"Passwords did not match."});
+        }
+      })
+      .catch( err => console.log(err))
+  }
+  if (!req.body.email || !req.body.password) {
+    res.status(401).json({message:"Password or username not provided."});
+  }
+});
+
+// REGISTER ROUTE
+router.post("/register", (req, res) => {
+  let hashPassword = bcrypt.hashSync(req.body.password, 10);
+  const user = {
+    first_name: req.body.firstname,
+    last_name: req.body.lastname,
+    email: req.body.email,
+    password: hashPassword
+  };
+  if( user.first_name && user.last_name && user.email && hashPassword){
+    Users.create(user)
+      .then((response) => {
+        const payload = {id: response.id};
+        const token = jwt.sign(payload, process.env.APP_SECRET_KEY);
+        res.json({
+          message: "Register Success from server",
+          token: token
+        })
+      })
+      .catch(err => {
+        if(err.errors.email.kind === 'unique') {
+          res.status(401).json({message: "That email is already used."})
+        } else {
+          res.status(401).json({message: "Registration has failed at the server."})
+        }
+      })
+  } else {
+    res.status(401).json({message: "Please complete all registration fields"})
+  }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,19 +3,17 @@
 const PORT        = process.env.PORT || 8080;
 const express     = require("express");
 const bodyParser  = require("body-parser");
-const mongoose   = require("mongoose");
-const jwt = require('jsonwebtoken');
-const passport = require("passport");
+const mongoose    = require("mongoose");
+const jwt         = require('jsonwebtoken');
+const passport    = require("passport");
 const passportJWT = require("passport-jwt");
 
-const Users       = require("./database/models/Users")
-
-const ExtractJwt = passportJWT.ExtractJwt;
+const ExtractJwt  = passportJWT.ExtractJwt;
 const JwtStrategy = passportJWT.Strategy;
-const bcrypt = require("bcrypt");
 require('dotenv').config();
+const Users       = require("./database/models/Users");
 
-const app = express();
+const app         = express();
 
 //MongoDB connection and configuration
 const dbuser = process.env.USERNAME;
@@ -28,13 +26,13 @@ mongoose.connect(uri, options);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Passport and JSON Web Token middleware
-const jwtOptions = {}
-  jwtOptions.jwtFromRequest = ExtractJwt.fromBodyField('token');
-  jwtOptions.secretOrKey = process.env.APP_SECRET_KEY;
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromBodyField('token'),
+  secretOrKey: process.env.APP_SECRET_KEY
+};
 const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
   const user = Users.findOne({'id': jwt_payload.id});
   if (user) {
@@ -54,70 +52,10 @@ app.use( (req, res, next) => {
 });
 
 //Mount all resource routes
-const apiRoutes    = require("./routes/routes");
+const apiRoutes    = require("./routes/apiRoutes");
+const userRoutes   = require("./routes/userRoutes");
 app.use("/api", passport.authenticate('jwt', { session: false }),  apiRoutes);
-
-
-//USER LOGIN ROUTE
-app.post("/login", (req, res) => {
-
-  if(req.body.email && req.body.password){
-    const email = req.body.email.toLowerCase();
-    const password = req.body.password;
-    Users.findOne({'email': email })
-    .then( (user) => {
-      if( !user) {
-        res.status(401).json({message:"No such user found."});
-      } else if( user && (bcrypt.compareSync(  password, user.password))) {
-        const payload = {id: user.id};
-        const token = jwt.sign(payload, jwtOptions.secretOrKey);
-        res.json({message: "Password OK.", token: token});
-      } else {
-        console.log(user.password);
-        console.log(password);
-        res.status(401).json({message:"Passwords did not match."});
-      }
-    })
-    .catch( err => console.log(err))
-  }
-  if (!req.body.email || !req.body.password) {
-    res.status(401).json({message:"Password or username not provided."});
-  }
-});
-
-// REGISTER ROUTE
-app.post("/register", (req, res) => {
-  let hashPassword = bcrypt.hashSync(req.body.password, 10);
-  const user = {
-    first_name: req.body.firstname,
-    last_name: req.body.lastname,
-    email: req.body.email,
-    password: hashPassword
-  };
-  if( user.first_name && user.last_name && user.email && hashPassword){
-    Users.create(user)
-      .then((response) => {
-        const payload = {id: response.id};
-        const token = jwt.sign(payload, jwtOptions.secretOrKey);
-        res.json({
-          message: "Register Success from server",
-          token: token
-        })
-      })
-      .catch(err => {
-        console.log(err.errors.email.kind);
-        if(err.errors.email.kind === 'unique') {
-          res.status(401).json({message: "That email is already used."})
-        } else {
-          res.status(401).json({message: "Registration has failed at the server."})
-        }
-      })
-  } else {
-    res.status(401).json({message: "Please complete all registration fields"})
-  }
-});
-
-
+app.use("/", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port: " + PORT);
